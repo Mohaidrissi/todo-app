85bb93a4a0f09de1762b3a043b2226b4d4a21d19
mohamed-elidrissi <mohamedel62840@gmail.com>
2026-07-11 13:36:42 +0100

    Ajout de la fonctionnalité de suppression de tâche

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ function addTask() {
 
     const input = document.getElementById("task");
 
+    if (input.value.trim() === "") {
+        return;
+    }
+
     const li = document.createElement("li");
 
     li.textContent = input.value;
+
+    li.addEventListener("click", function () {
+        li.remove();
+    });
 
     document.getElementById("list").appendChild(li);
 
